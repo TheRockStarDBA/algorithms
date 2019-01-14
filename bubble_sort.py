61a0a5677a08169b3a83e049218b036d2721db8b
Nan Li <nanli-7@users.noreply.github.com>
2019-01-13 22:34:40 -0500

    bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,13 @@
 """ Bubble Sort
+
 Topics: search and sorting
 
-This is a very simple sorting algorithm. Because it's also very inefficient, Bubble Sort is not practical for real-world use and is generally only discussed in an academic context. The basic theory behind BubbleSort is that you take an array of integers and iterate through it; for each element at some index  whose value is greater than the element at the index following it (i.e., index i+1 ), you must swap the two values. The act of swapping these values causes the larger, unsorted values to float to the back (like a bubble) of the data structure until they land in the correct location.
+This is a very simple sorting algorithm. Because it's also very inefficient, Bubble Sort is not practical
+for real-world use and is generally only discussed in an academic context. The basic theory behind BubbleSort
+is that you take an array of integers and iterate through it; for each element at some index  whose value
+is greater than the element at the index following it (i.e., index i+1 ), you must swap the two values.
+The act of swapping these values causes the larger, unsorted values to float to the back (like a bubble)
+of the data structure until they land in the correct location.
 
 Asymptotic Analysis
 Worst Case: O(n^2)

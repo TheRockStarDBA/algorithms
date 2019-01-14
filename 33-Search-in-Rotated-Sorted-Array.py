20e567c09b4a8c271a45c6f2f7035f20f7d76ab2
Nan Li <nanli-7@users.noreply.github.com>
2019-01-14 17:52:25 -0500

    updates

--- 33-Search-in-Rotated-Sorted-Array.py
+++ 33-Search-in-Rotated-Sorted-Array.py
@@ -1,18 +1,42 @@
-# Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+"""
+33. Search in Rotated Sorted Array (Medium)
+Topic: binary-search, array;
 
-# (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
-# You are given a target value to search. If found in the array return its index, otherwise return -1.
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
 
-# You may assume no duplicate exists in the array.
+You are given a target value to search. If found in the array return its index, otherwise return -1.
 
-# Your algorithm's runtime complexity must be in the order of O(log n).
+You may assume no duplicate exists in the array.
 
-# Example 1:
+Your algorithm's runtime complexity must be in the order of O(log n).
 
-# Input: nums = [4,5,6,7,0,1,2], target = 0
-# Output: 4
-# Example 2:
+Example 1:
 
-# Input: nums = [4,5,6,7,0,1,2], target = 3
-# Output: -1
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1 """
+
+# Approach : Binary search
+# The problem is to implement a search in O(log(N)) time that gives an idea to use a binary search.
+
+# The algorithm is quite straightforward :
+
+# Find a rotation index rotation_index, i.e. index of the smallest element in the array. Binary search works just perfect here.
+
+# rotation_index splits array in two parts. Compare nums[0] and target to identify in which part one has to look for target.
+
+# Perform a binary search in the chosen part of the array.
+
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """

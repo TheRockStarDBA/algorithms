4962d31c4eb01f82809691e9bbf8e63816038931
Nan Li <nanli-7@users.noreply.github.com>
2019-01-14 18:38:42 -0500

    binary search

--- 33-Search-in-Rotated-Sorted-Array.py
+++ 33-Search-in-Rotated-Sorted-Array.py
@@ -1,6 +1,6 @@
 """
-33. Search in Rotated Sorted Array (Medium)
-Topic: binary-search, array;
+33. Search in Rotated Sorted Array
+Topic: binary-search, array; (Medium Level)
 
 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
@@ -19,7 +19,8 @@ Output: 4
 Example 2:
 
 Input: nums = [4,5,6,7,0,1,2], target = 3
-Output: -1 """
+Output: -1
+"""
 
 # Approach : Binary search
 # The problem is to implement a search in O(log(N)) time that gives an idea to use a binary search.
@@ -40,3 +41,55 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+
+        def find_rotation_index(left, right):
+            if nums[left] < nums[right]:
+                return 0
+
+            while left <= right:
+                pivot = (left + right) // 2
+                if nums[pivot] > nums[pivot + 1]:
+                    return pivot + 1
+                else:
+                    if nums[pivot] < nums[left]:
+                        right = pivot - 1
+                    else:
+                        left = pivot + 1
+
+        def search_target(left, right):
+            # Binary search
+            while left <= right:
+                pivot = (left + right) // 2
+                if nums[pivot] == target:
+                    return pivot
+                else:
+                    if target < nums[pivot]:
+                        right = pivot - 1
+                    else:
+                        left = pivot + 1
+            return -1
+
+        n = len(nums)
+
+        if n == 0:
+            return -1
+        if n == 1:
+            return 0 if nums[0] == target else -1
+
+        rotate_index = find_rotation_index(0, n - 1)
+
+        #if target is the smallest number
+        if nums[rotate_index] == target:
+            return rotate_index
+        #if array is not rotated, search the entire array:
+        if rotate_index == 0:
+            return search_target(0, n - 1)
+        if target < nums[0]:
+            # search on the right side
+            return search_target(rotate_index, n - 1)
+        # search on the left side
+        return search_target(0, rotate_index)
+
+
+print(Solution().search(nums=[4, 5, 6, 7, 0, 1, 2], target=0))
+print(Solution().search(nums=[4, 5, 6, 7, 0, 1, 2], target=3))

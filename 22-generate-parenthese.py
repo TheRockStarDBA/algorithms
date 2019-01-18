f78f6582b92721d150a6f1007407985c6fdd1dc8
Nan Li <nanli-7@users.noreply.github.com>
2019-01-18 13:12:33 -0500

    update solution

--- 22-generate-parenthese.py
+++ 22-generate-parenthese.py
@@ -1,11 +1,48 @@
-# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+"""
+22. Generate Parentheses
 
-# For example, given n = 3, a solution set is:
+Topics: string, backtracking, recursion (medium level)
+Similar Questions: Letter Combinations of a Phone Number (Medium); Valid Parentheses (Easy)
 
-# [
-#   "((()))",
-#   "(()())",
-#   "(())()",
-#   "()(())",
-#   "()()()"
-# ]
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+] """
+
+# Approach: Backtracking
+
+# Add '(' or ')' when we know it will remain a valid sequence. We can do this by keeping track of the number of opening and closing brackets we have placed so far.
+
+# We can start an opening bracket if we still have one (of n) left to place. And we can start a closing bracket if it would not exceed the number of opening brackets.
+
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+
+        ans = []
+
+        def backtrack(s='', left=0, right=0):
+            if len(s) == 2 * n:
+                ans.append(s)
+                return
+            if left < n:
+                backtrack(s + '(', left + 1, right)
+            if right < left:
+                backtrack(s + ')', left, right + 1)
+
+        backtrack()
+        return ans
+
+
+print(Solution().generateParenthesis(3))
--- 5-longest-palindrome-substring.py
+++ 5-longest-palindrome-substring.py
@@ -20,27 +20,31 @@ Example 2:
 Input: "cbbd"
 Output: "bb" """
 
+
 class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        if len(s)==0:
-        	return ""
-        maxLen=1
-        start=0
-        for i in xrange(len(s)):
-        	if i-maxLen >=1 and s[i-maxLen-1:i+1]==s[i-maxLen-1:i+1][::-1]:
-        		start=i-maxLen-1
-        		maxLen+=2
-        		continue
+        if len(s) == 0:
+            return ""
+        maxLen = 1
+        start = 0
+        for i in range(len(s)):
+            if i - maxLen >= 1 and s[i - maxLen - 1:i + 1] == s[i - maxLen -
+                                                                1:i + 1][::-1]:
+                start = i - maxLen - 1
+                maxLen += 2
+                continue
 
-        	if i-maxLen >=0 and s[i-maxLen:i+1]==s[i-maxLen:i+1][::-1]:
-        		start=i-maxLen
-        		maxLen+=1
-        return s[start:start+maxLen]
+            if i - maxLen >= 0 and s[i - maxLen:i + 1] == s[i - maxLen:i +
+                                                            1][::-1]:
+                start = i - maxLen
+                maxLen += 1
+        return s[start:start + maxLen]
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     result = Solution().longestPalindrome("babad")
     print(result)

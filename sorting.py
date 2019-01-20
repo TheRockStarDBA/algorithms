0e4329e235d5899335d765fae0f84bafde8468cb
Nan Li <nanli-7@users.noreply.github.com>
2019-01-20 16:42:52 -0500

    divide-and-conquer algorithms

--- sorting.py
+++ sorting.py
@@ -1,7 +1,5 @@
 """ Bubble Sort
 
-Topics: search and sorting
-
 This is a very simple sorting algorithm. Because it's also very inefficient, Bubble Sort is not practical
 for real-world use and is generally only discussed in an academic context. The basic theory behind BubbleSort
 is that you take an array of integers and iterate through it; for each element at some index  whose value
@@ -40,3 +38,19 @@ arr = [64, 34, 25, 12, 22, 11, 90]
 bubbleSort(arr)
 
 print("Sorted array is:", arr)
+""" Merge Sort
+
+Topics: search and sorting
+
+This is a very simple sorting algorithm. Because it's also very inefficient, Bubble Sort is not practical
+for real-world use and is generally only discussed in an academic context. The basic theory behind BubbleSort
+is that you take an array of integers and iterate through it; for each element at some index  whose value
+is greater than the element at the index following it (i.e., index i+1 ), you must swap the two values.
+The act of swapping these values causes the larger, unsorted values to float to the back (like a bubble)
+of the data structure until they land in the correct location.
+
+Asymptotic Analysis
+Worst Case: O(n^2)
+Best Case: O(n)
+Average: O(n^2)
+ """

28ac04c1bc77834fa5ae3c65c66842168c3e5c44
Nan Li <nanli-7@users.noreply.github.com>
2019-01-23 16:37:15 -0500

    merge intervals by sorting

---
+++ 56-merge-intervals.py
@@ -0,0 +1,86 @@
+""" 56. Merge Intervals
+
+Topic: Array, Sort (Medium)
+Related: Insert Intervals - Hard
+        Meeting Rooms - Easy
+        Meeting Rooms II - Medium
+        Teemo Attacking - Medium
+        Add Bold Tag in String - Medium
+        Range Module - Hard
+        Employee Free Time - Hard
+        Partition Labels - Medium
+
+Given a collection of intervals, merge all overlapping intervals.
+
+Example 1:
+
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+Example 2:
+
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping. """
+""" Approach: Sorting
+
+Intuition:
+If we sort the intervals by their start value, then each set of intervals that can be merged will appear as a contiguous
+ "run" in the sorted list.
+
+Algorithm
+
+First, we sort the list as described. Then, we insert the first interval into our merged list and continue considering
+each interval in turn as follows: If the current interval begins after the previous interval ends, then they do not
+overlap and we can append the current interval to merged. Otherwise, they do overlap, and we merge them by updating the
+end of the previous interval if it is less than the end of the current interval.
+
+A simple proof by contradiction shows that this algorithm always produces the correct answer. First, suppose that the
+algorithm at some point fails to merge two intervals that should be merged. This would imply that there exists some
+triple of indices i, j, and k in a list of intervals intsints such that i<j<k and (ints[i], ints[k]) can be merged, but
+neither (ints[i], ints[j]) nor (ints[j], ints[k]) can be merged. From this scenario follow several inequalities:
+
+ints[i].end<ints[j].start
+ints[j].end<ints[k].start
+ints[i].end≥ints[k].start
+​
+We can chain these inequalities (along with the following inequality, implied by the well-formedness of the intervals:
+ints[j].start ≤ ints[j].end) to demonstrate a contradiction:
+
+ints[i].end<ints[j].start≤ints[j].end<ints[k].start
+ints[i].end≥ints[k].start
+​
+Therefore, all mergeable intervals must occur in a contiguous run of the sorted list. """
+
+
+# Definition for an interval.
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        if not intervals:
+            return intervals
+
+        intervals.sort(key=lambda x: x.start)
+
+        merged = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not merged or merged[-1].end < interval.start:
+                merged.append(interval)
+            else:
+                # otherwise, there is overlap, so we merge the current and
+                # previous intervals.
+                merged[-1].end = max(merged[-1].end, interval.end)
+
+        return merged

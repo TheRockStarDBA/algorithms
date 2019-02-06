b998c8e53178a5e0a89007954815dfbee65d51e4
Nan Li <nanli-7@users.noreply.github.com>
2019-02-06 18:07:20 -0500

    binary search

--- 74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix.py
@@ -1,13 +1,15 @@
 """ 74. Search a 2D Matrix - Medium
 
-topic: array, binary search
+Topic: array, binary search
+Related: Search a 2D Matrix II - Medium
 
-Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+Write an efficient algorithm that searches for a value in an m x n matrix. This
+matrix has the following properties:
 
 Integers in each row are sorted from left to right.
 The first integer of each row is greater than the last integer of the previous row.
-Example 1:
 
+Example 1:
 Input:
 matrix = [
   [1,   3,  5,  7],
@@ -16,8 +18,8 @@ matrix = [
 ]
 target = 3
 Output: true
-Example 2:
 
+Example 2:
 Input:
 matrix = [
   [1,   3,  5,  7],
@@ -28,6 +30,8 @@ target = 13
 Output: false """
 
 
+# Time:  O(logm + logn)
+# Space: O(1)
 class Solution(object):
     def searchMatrix(self, matrix, target):
         """
@@ -35,3 +39,21 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
+        if not matrix:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n
+        while left < right:
+            mid = left + (right - left) / 2
+            if matrix[mid / n][mid % n] >= target:
+                right = mid
+            else:
+                left = mid + 1
+        return left < m * n and matrix[left / n][left % n] == target
+
+
+if __name__ == "__main__":
+    matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50]]
+    target = 3
+    print(Solution().searchMatrix(matrix, target))

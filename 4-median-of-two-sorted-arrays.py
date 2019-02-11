7d67d7acfff95211667f2618e638a62a17b16e63
Nan Li <nanli-7@users.noreply.github.com>
2019-02-10 21:23:16 -0500

    binary search

--- 4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays.py
@@ -2,10 +2,10 @@
 
 Topic: array, binary search, divide and conquer
 
-
 There are two sorted arrays nums1 and nums2 of size m and n respectively.
 
-Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+Find the median of the two sorted arrays. The overall run time complexity should
+be O(log (m+n)).
 
 You may assume nums1 and nums2 cannot be both empty.
 
@@ -23,6 +23,9 @@ nums2 = [3, 4]
 The median is (2 + 3)/2 = 2.5 """
 
 
+# Binary Search Approach
+# Time:  O(log(min(m, n)))
+# Space: O(1)
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -30,3 +33,33 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
+        len1, len2 = len(nums1), len(nums2)
+        k = (len1 + len2) // 2
+        if (len1 + len2) % 2 == 1:
+            return self.getKth(nums1, nums2, k + 1)
+        else:
+            return (self.getKth(nums1, nums2, k + 1) + self.getKth(
+                nums1, nums2, k)) / 2.0
+
+    def getKth(self, A, B, k):
+        m, n = len(A), len(B)
+        if m > n:
+            return self.getKth(B, A, k)
+
+        left, right = 0, m
+        while left < right:
+            mid = left + (right - left) // 2
+            if 0 <= k - mid - 1 < n and A[mid] >= B[k - mid - 1]:
+                right = mid
+            else:
+                left = mid + 1
+
+        Ai = A[left - 1] if left - 1 >= 0 else float("-inf")
+        Bj = B[k - 1 - left] if k - 1 - left >= 0 else float("-inf")
+
+        return max(Ai, Bj)
+
+
+if __name__ == "__main__":
+    print(Solution().findMedianSortedArrays([1, 2], [1, 2, 3]))
+    print(Solution().findMedianSortedArrays([1, 2], [3, 4]))

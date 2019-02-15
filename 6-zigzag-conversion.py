0a100b785d5596a85511b309e11606e534ce5e7b
Nan Li <nanli-7@users.noreply.github.com>
2019-02-15 11:07:32 -0500

    string

--- 6-zigzag-conversion.py
+++ 6-zigzag-conversion.py
@@ -1,7 +1,7 @@
 """ 6. ZigZag Conversion - Medium
 
-The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of 
-rows like this: (you may want to display this pattern in a fixed font for better 
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of
+rows like this: (you may want to display this pattern in a fixed font for better
 legibility)
 
 P   A   H   N
@@ -9,7 +9,7 @@ A P L S I I G
 Y   I   R
 And then read line by line: "PAHNAPLSIIGYIR"
 
-Write the code that will take a string and make this conversion given a number 
+Write the code that will take a string and make this conversion given a number
 of rows:
 
 string convert(string s, int numRows);
@@ -36,3 +36,20 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
+        if numRows == 1:
+            return s
+
+        step = 2 * numRows - 2
+        zig = ''
+
+        for i in range(numRows):
+            for j in range(i, len(s), step):
+                zig += s[j]
+                if 0 < i < numRows - 1 and (j + step - 2 * i) < len(s):
+                    zig += s[j + step - 2 * i]
+        return zig
+
+
+if __name__ == "__main__":
+    result = Solution().convert(s="PAYPALISHIRING", numRows=3)
+    print(result)

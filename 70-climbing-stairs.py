6ab0f89daaf4d33803f3a01d02237c1e9bf690cd
Nan Li <nanli-7@users.noreply.github.com>
2019-02-16 20:25:40 -0500

    dynamic programming

---
+++ 70-climbing-stairs.py
@@ -0,0 +1,49 @@
+""" 70. Climbing Stairs - Easy
+
+topic: dynamic programming
+related:    Min Cost Climbing Stairs - Easy
+            Fibonacci Number - Easy
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you
+climb to the top?
+
+Note: Given n will be a positive integer.
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step """
+
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return n
+
+        dp = [1 for _ in range(n)]
+        dp[1] += dp[0]
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[-1]
+
+
+if __name__ == "__main__":
+    print(Solution().climbStairs(6))

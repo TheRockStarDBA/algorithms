756cb38ed2880521157297c92e5759c8029068c7
Nan Li <nanli-7@users.noreply.github.com>
2019-03-30 22:35:58 -0400

    rotate array

--- 189-rotate-array.py
+++ 189-rotate-array.py
@@ -27,6 +27,27 @@ Try to come up as many solutions as you can, there are at least 3 different ways
 to solve this problem.
 Could you do it in-place with O(1) extra space? """
 
+# Approach 1: Using Reverse
+# Algorithm
+
+# This approach is based on the fact that when we rotate the array k times, k
+# elements from the back end of the array come to the front and the rest of the
+# elements from the front shift backwards.
+
+# In this approach, we firstly reverse all the elements of the array. Then,
+# reversing the first k elements followed by reversing the rest n-k elements
+# gives us the required result.
+
+# Let n=7 and k=3.
+
+# Original List                   : 1 2 3 4 5 6 7
+# After reversing all numbers     : 7 6 5 4 3 2 1
+# After reversing first k numbers : 5 6 7 4 3 2 1
+# After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
+
+# Time:  O(n)
+# Space: O(1)
+
 
 class Solution(object):
     def rotate(self, nums, k):
@@ -35,3 +56,20 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        k %= len(nums)
+        self.reverse(nums, 0, len(nums))
+        self.reverse(nums, 0, k)
+        self.reverse(nums, k, len(nums))
+
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end-1] = nums[end-1], nums[start]
+            start += 1
+            end -= 1
+
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    k = 3
+    Solution().rotate(nums, k)
+    print(nums)

c073a6ffcd47e3105df813003acd7c88c64b359a
Nan Li <nanli-7@users.noreply.github.com>
2019-04-01 11:49:10 -0400

    greedy

--- 44-wildcard-matching.py
+++ 44-wildcard-matching.py
@@ -57,3 +57,38 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
+        p_idx, s_idx, last_p_idx, last_s_idx = 0, 0, -1, -1
+
+        while s_idx < len(s):
+            # regular matching with '?'
+            if p_idx < len(p) and (s[s_idx] == p[p_idx] or p[p_idx] == "?"):
+                s_idx += 1
+                p_idx += 1
+
+            # matching with '*'
+            elif p_idx < len(p) and p[p_idx] == "*":
+                p_idx += 1
+                last_p_idx = p_idx
+                last_s_idx = s_idx
+
+            # Not matching, but there is a '*' in previous
+            elif last_p_idx != -1:
+                last_s_idx += 1
+                s_idx = last_s_idx
+                p_idx = last_p_idx
+
+            # Not matching and there is no '*' in previous
+            else:
+                return False
+
+        # Check if there is still character except '*' in the pattern
+        while p_idx < len(p) and p[p_idx] == '*':
+            p_idx += 1
+
+        # If finish scanning both string and pattern, then it matches well
+        return p_idx == len(p)
+
+
+if __name__ == "__main__":
+    print(Solution().isMatch(s="acdcb", p="a*c?b"))
+    print(Solution().isMatch(s="adceb", p="*a*b"))

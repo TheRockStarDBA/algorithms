fb5109cc01c7df411ae13ef8de0758bc61ccb16a
Nan Li <nanli-7@users.noreply.github.com>
2019-03-31 20:47:00 -0400

    dynamic programming

--- 10-regular-expression-matching.py
+++ 10-regular-expression-matching.py
@@ -51,11 +51,78 @@ s = "mississippi"
 p = "mis*is*p*."
 Output: false """
 
+# Approach 1: Recursion
+# Algorithm
+
+# If there were no Kleene stars (the * wildcard character for regular expressions),
+# the problem would be easier - we simply check from left to right if each
+# character of the text matches the pattern.
+
+# When a star is present, we may need to check many different suffixes of the
+# text and see if they match the rest of the pattern. A recursive solution is a
+# straightforward way to represent this relationship.
+
 
 class Solution(object):
-    def isMatch(self, s, p):
+    def isMatch(self, text, pattern):
         """
-        :type s: str
-        :type p: str
+        :type text: str
+        :type pattern: str
         :rtype: bool
         """
+        if not pattern:
+            return not text
+
+        first_match = bool(text) and pattern[0] in {text[0], "."}
+
+        if len(pattern) >= 2 and pattern[1] == "*":
+            return (self.isMatch(text, pattern[2:])
+                    or first_match and self.isMatch(text[1:], pattern))
+
+        else:
+            return first_match and self.isMatch(text[1:], pattern[1:])
+
+
+# Approach 2: Dynamic Programming
+# Intuition
+
+# As the problem has an optimal substructure, it is natural to cache intermediate
+# results. We ask the question dp(i, j): does text[i:] and pattern[j:] match? We
+# can describe our answer in terms of answers to questions involving smaller strings.
+
+# Algorithm
+
+
+# We proceed with the same recursion as in Approach 1, except because calls will
+# only ever be made to match(text[i:], pattern[j:]), we use dp(i, j) to handle
+# those calls instead, saving us expensive string-building operations and allowing
+# us to cache the intermediate results.
+class Solution2(object):
+    def isMatch(self, text, pattern):
+        """
+        :type text: str
+        :type pattern: str
+        :rtype: bool
+        """
+        dp = [[False] * (len(pattern) + 1) for _ in range(len(text) + 1)]
+        dp[0][0] = True
+        for i in range(2, len(pattern) + 1):
+            if pattern[i - 1] == '*':
+                dp[0][i] = dp[0][i - 2]
+
+        for i in range(1, len(text) + 1):
+            for j in range(1, len(pattern) + 1):
+                if pattern[j - 1] != '*':
+                    dp[i][j] = dp[i - 1][j - 1] and (
+                        text[i - 1] == pattern[j - 1] or pattern[j - 1] == '.')
+                else:
+                    dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] and
+                                                (text[i - 1] == pattern[j - 2]
+                                                 or pattern[j - 2] == '.'))
+
+        return dp[len(text)][len(pattern)]
+
+
+if __name__ == "__main__":
+    res = Solution().isMatch(text="mississippi", pattern="mis*is*p*.")
+    print(res)

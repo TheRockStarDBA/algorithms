d02e155ac4dc58ac06344dff7896c0a311868d5f
Nan Li <nanli-7@users.noreply.github.com>
2019-04-15 18:07:50 -0400

    tree, breadth first search

--- 102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal.py
@@ -1,5 +1,8 @@
 """ 102. Binary Tree Level Order Traversal - Medium
-Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+#tree #breadth-first search
+
+Given a binary tree, return the level order traversal of its nodes' values. (ie,
+from left to right, level by level).
 
 For example:
 Given binary tree [3,9,20,null,null,15,7],
@@ -15,12 +18,13 @@ return its level order traversal as:
   [15,7]
 ] """
 
+
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
 class Solution(object):
@@ -29,3 +33,18 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        if not root:
+            return []
+        result, current = [], [root]
+        while current:
+            next_level, curr_level_vals = [], []
+            for node in current:
+                curr_level_vals.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            current = next_level
+            result.append(curr_level_vals)
+
+        return result

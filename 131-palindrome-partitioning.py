2443bb1828a31f59f3254bd2b343b9fe1100791e
Nan Li <nanli-7@users.noreply.github.com>
2019-05-03 19:02:50 -0400

    backtracking

--- 131-palindrome-partitioning.py
+++ 131-palindrome-partitioning.py
@@ -22,3 +22,25 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
+        result = []
+        current = []
+        self.partitionRecursive(result, current, s, 0)
+        return result
+
+    def partitionRecursive(self, result, current, s, start):
+        if start == len(s):
+            result.append(list(current))
+        else:
+            for i in range(start, len(s)):
+                if self.isPalindrome(s[start:i + 1]):
+                    current.append(s[start:i + 1])
+                    self.partitionRecursive(result, current, s, i + 1)
+                    current.pop()
+
+    def isPalindrome(self, substring):
+        return substring == substring[::-1]
+
+
+if __name__ == "__main__":
+    res = Solution().partition(s='aab')
+    print(res)

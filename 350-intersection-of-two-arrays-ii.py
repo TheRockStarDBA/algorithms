dacd44399364a913f26535b183f3b661e09f29ef
Nan Li <nanli-7@users.noreply.github.com>
2019-05-18 22:18:58 -0400

    binary search, two pointers

---
+++ 350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,61 @@
+""" 350. Intersection of Two Arrays II - Easy
+##hash table ##binary search ##two pointers ##sort
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Note:
+
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+ """
+
+# If the given array is not sorted, and the memory is limited.
+# Time:  O(max(m, n) * log(max(m, n)))
+# Space: O(1)
+# Two pointers approach.
+
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort()
+        nums2.sort()
+        res = []
+        i, j = 0, 0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+
+        return res
+
+
+if __name__ == "__main__":
+    nums1 = [4, 9, 5]
+    nums2 = [9, 4, 9, 8, 4]
+    res = Solution().intersect(nums1, nums2)
+    print(res)

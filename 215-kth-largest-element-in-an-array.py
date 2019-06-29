71f7f82695a692cb369e41368c8f60c05f8750ac
Nan Li <nanli-7@users.noreply.github.com>
2019-06-29 17:08:56 -0400

    quick sort, devide conquer

--- 215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array.py
@@ -16,6 +16,8 @@ You may assume k is always valid, 1 ≤ k ≤ array's length.
 
  """
 
+import random
+
 
 class Solution(object):
     def findKthLargest(self, nums, k):
@@ -24,5 +26,29 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot_idx = random.randint(left, right)
+            pos = self.partition(left, right, pivot_idx, nums)
+            if pos == k - 1:
+                return nums[pos]
+            elif pos > k - 1:
+                right = pos - 1
+            else:
+                left = pos + 1
+
+    def partition(self, left, right, pivot_idx, nums):
+        pivot_val = nums[pivot_idx]
+        new_pivot_idx = left
+        nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+        for i in range(left, right):
+            if nums[i] > pivot_val:
+                nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
+                new_pivot_idx += 1
+        nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
+        return new_pivot_idx
 
 
+if __name__ == "__main__":
+    res = Solution().findKthLargest(nums=[3, 2, 1, 5, 6, 4], k=2)
+    print(res)
